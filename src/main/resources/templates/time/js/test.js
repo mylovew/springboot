352acde583a2fa18468a4d1a3050ceae154ceadb
wkzhang <1486919714@qq.com>
2019-07-23 16:38:31 +0800

    排程滚动条设置

--- src/main/resources/templates/time/js/test.js
+++ src/main/resources/templates/time/js/test.js
@@ -1,5 +1,13 @@
 jQuery(document).ready(function(){
+    //$(".right").css("width",(800-135)+"px");
+
     jQuery('.scrollbar-inner').scrollbar();
+    jQuery('.scrollbar-dynamic').scrollbar({
+        "onScroll":function (y,x) {
+            //console.log(x);
+        }
+    });
+
     function body(hallNumber) {
 
     }
