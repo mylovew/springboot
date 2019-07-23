204fe1285b1bbe6e11ea49d239ca92f4d37efed2
wkzhang <1486919714@qq.com>
2019-07-23 17:42:26 +0800

    排程滚动条设置

--- src/main/resources/templates/time/js/test.js
+++ src/main/resources/templates/time/js/test.js
@@ -1,13 +1,21 @@
 jQuery(document).ready(function(){
     //$(".right").css("width",(800-135)+"px");
 
-    jQuery('.scrollbar-inner').scrollbar();
+    jQuery('.scrollbar-inner').scrollbar({
+        "onScroll":function (y,x) {
+            jQuery('.scroll-element.scroll-x.scroll-scrollx_visible').position({
+                of: $( ".timeBody" ),
+                at: "center+45 bottom-8"
+            });
+        }
+    });
     jQuery('.scrollbar-dynamic').scrollbar({
         "onScroll":function (y,x) {
             //console.log(x);
         }
     });
 
+    console.log(jQuery('.scroll-element .scroll-x .scroll-scrollx_visible').text());
     function body(hallNumber) {
 
     }
