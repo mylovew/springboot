233f94524f100f931fa0504a320805da5e4de5f8
wkzhang <1486919714@qq.com>
2019-07-24 15:41:51 +0800

    时间线和时间刻度 位置定位

--- src/main/resources/templates/time/js/test.js
+++ src/main/resources/templates/time/js/test.js
@@ -1,21 +1,36 @@
 jQuery(document).ready(function(){
     //$(".right").css("width",(800-135)+"px");
+    function scheduling(divClass){
+        //初始化时间线高度
+        $('.timeLine').height($('.right').height()-35);
 
-    jQuery('.scrollbar-inner').scrollbar({
-        "onScroll":function (y,x) {
-            jQuery('.scroll-element.scroll-x.scroll-scrollx_visible').position({
-                of: $( ".timeBody" ),
-                at: "center+45 bottom-8"
-            });
-        }
-    });
-    jQuery('.scrollbar-dynamic').scrollbar({
-        "onScroll":function (y,x) {
-            //console.log(x);
-        }
-    });
+        jQuery('.scrollbar-dynamic').scrollbar({
+            "onScroll":function (y,x) {
+                //console.log(x);
 
-    console.log(jQuery('.scroll-element .scroll-x .scroll-scrollx_visible').text());
+            }
+        });
+        jQuery('.scrollbar-inner').scrollbar({
+            "onScroll":function (y,x) {
+                if(y.maxScroll > 0){
+                    //表示有滚动条
+                    //底部横向栏固定
+                    jQuery('.scroll-element.scroll-x.scroll-scrollx_visible').position({
+                        of: $( "."+divClass ),
+                        at: "center+46 bottom-8"
+                    });
+                    var num = ($('.time').width() - $( "."+divClass ).width())/2 + $('.left').width();
+                    jQuery('.time').position({
+                        of: $( "."+divClass ),
+                        at: "center+"+num+" top+18"
+                    });
+                }
+
+            }
+        });
+
+    }
+    scheduling("timeBody");
     function body(hallNumber) {
 
     }
